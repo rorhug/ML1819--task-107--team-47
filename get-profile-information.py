0cce3276cda20b0d15fb968613ad7ac76c6c6adb
Rory Hughes <RoryDH@users.noreply.github.com>
2018-10-27 17:35:02 +0100

    Add extra users and gender detection

--- get-profile-information.py
+++ get-profile-information.py
@@ -1,11 +1,29 @@
 # settings.py
 import os
+import sys
 import csv
 import json
-
+import pdb
 
 from dotenv import load_dotenv
 import twitter
+import gender_guesser.detector as gender
+
+# DATASET DEFINITION
+
+# JSON file, key as their screen_name/user_name, e.g.
+# {
+#     screen_name: {
+#         gender: "male|female|mostly_male|brand|unknown",
+#         gender_source: "namesfile|",
+#         twitter_profile: {},
+#         tweets: [{}]
+#     }
+# }
+
+# maybe sleep_on_rate_limit=True
+
+
 
 load_dotenv()
 
@@ -13,39 +31,107 @@ api = twitter.Api(
     consumer_key=os.getenv("TWITTER_CONSUMER_KEY"),
     consumer_secret=os.getenv("TWITTER_CONSUMER_SECRET"),
     access_token_key=os.getenv("TWITTER_ACCESS_TOKEN_KEY"),
-    access_token_secret=os.getenv("TWITTER_ACCESS_TOKEN_SECRET")
+    access_token_secret=os.getenv("TWITTER_ACCESS_TOKEN_SECRET"),
+    sleep_on_rate_limit=True
 )
+
+DATASET_FILE = "users.json"
 
 def chunks(l, n):
     for i in range(0, len(l), n):
         yield l[i:i+n]
 
-def get_profiles_from_usernames(file_path):
-    users = {}
+def lowered_screen_names(users):
+    return map(lambda screen_name: screen_name.lower(), list(users.keys()))
 
-    # <screen_name>: { gender: g, twitter_profile: {}, tweets: {} }
-
-    with open(file_path, 'r') as csvfile:
+def extend_users(users, csv_file_path):
+    with open(csv_file_path, 'r') as csvfile:
         reader = csv.reader(csvfile, delimiter=',')
+
+        # pdb.set_trace()
+
+        # added_count = 0
+
         for row in reader:
-            users[row[0].lower()] = { "gender": row[1] }
+            screen_name = row[0].lower()
+            gender_column_present = len(row) > 1
+            if screen_name not in users:
+                users[screen_name] = { "gender": row[1], "gender_source": "" } if gender_column_present else {}
 
-    for usernames_chunk in chunks(list(users.keys()), 100):
-        response = api.UsersLookup(screen_name=usernames_chunk, include_entities=True, return_json=True)
+    return True
 
-        for user_dict in response:
-            users[user_dict["screen_name"].lower()]["twitter_profile"] = user_dict
+
+def populate_twitter_profiles(users):
+    screen_names_to_query = [screen_name for screen_name in lowered_screen_names(users) if "twitter_profile" not in users[screen_name]]
+
+    page_count = 0
+
+    for screen_names_chunk in chunks(screen_names_to_query, 100):
+        page_count += 1
+        print(f"getting page: {page_count} of profiles")
+        try:
+            user_responses = api.UsersLookup(screen_name=screen_names_chunk, include_entities=True, return_json=True)
+            print(f"got {len(user_responses)}/{len(screen_names_chunk)} profiles")
+            for user_dict in user_responses:
+                users[user_dict["screen_name"].lower()]["twitter_profile"] = user_dict
+        except twitter.error.TwitterError as e:
+            print("can't get users ")
+            print(e)
 
     for screen_name in list(users.keys()):
         if not 'twitter_profile' in users[screen_name]:
             users.pop(screen_name, None)
 
-    return users
+    return True
+
+def populate_user_tweets(users):
+    screen_names_to_query = [screen_name for screen_name in lowered_screen_names(users) if "tweets" not in users[screen_name]]
+
+    print(f"getting tweets for {len(screen_names_to_query)} users")
+    user_count = 0
+
+    for screen_name in screen_names_to_query:
+        user_count += 1
+        try:
+            if user_count % 100 is 0: print(f"getting tweets for user {user_count}")
+            tweets = api.GetUserTimeline(screen_name=screen_name, count=200, include_rts=False, trim_user=True, exclude_replies=False)
+            users[screen_name]["tweets"] = [tweet.AsDict() for tweet in tweets]
+        except twitter.error.TwitterError as e:
+            print("can't get timeline for " + screen_name)
+            print(e)
 
 
-def write_users(users):
-    with open('users.json', 'w') as outfile:
-        json.dump(users, outfile)
+def populate_genders(users):
+    d = gender.Detector()
+    for screen_name in list(users.keys()):
+        user = users[screen_name]
+        if ("gender" not in user) and ("twitter_profile" in user) and ("name" in user["twitter_profile"]):
+            user["gender"] = d.get_gender(user["twitter_profile"]["name"].split(" ")[0])
+            user["gender_source"] = "gender-guesser"
+
+def write_dataset(users, file):
+    file.truncate(0)
+    json.dump(users, file)
 
 
+def main():
+    with open(DATASET_FILE, "a+") as dataset_file:
+        dataset_file.seek(0)
+        users = json.load(dataset_file)
 
+        # pdb.set_trace()
+        print(sys.argv)
+        if len(sys.argv) > 1:
+            extend_users(users, sys.argv[1])
+
+        populate_twitter_profiles(users)
+        write_dataset(users, dataset_file)
+
+        # populate_user_tweets(users)
+        # write_dataset(users, dataset_file)
+
+        populate_genders(users)
+        write_dataset(users, dataset_file)
+
+if __name__== "__main__":
+    main()
